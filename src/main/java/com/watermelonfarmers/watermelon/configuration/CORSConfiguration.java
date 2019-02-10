ccccef4a674915f9ae787c4372bff52de180bab4
Bryon Ingemi <bingemi@gmail.com>
2019-02-10 12:35:40 -0500

    add cors configuration

---
+++ src/main/java/com/watermelonfarmers/watermelon/configuration/CORSConfiguration.java
@@ -0,0 +1,28 @@
+package com.watermelonfarmers.watermelon.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSConfiguration extends CorsFilter
+{
+    public CORSConfiguration() {
+        super(configurationSource());
+    }
+
+    private static UrlBasedCorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
