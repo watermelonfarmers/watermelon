92ccbf5b910c122734d13af24f1a24db537aab52
TargaryenQi <44879154+TargaryenQi@users.noreply.github.com>
2019-02-23 23:12:01 -0500

    revert back

--- src/main/java/com/watermelonfarmers/watermelon/controllers/UserController.java
+++ src/main/java/com/watermelonfarmers/watermelon/controllers/UserController.java
@@ -24,8 +24,6 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers() {
 
-        // github work flow test
-
         return userProcessor.getUsers();
 
     }
