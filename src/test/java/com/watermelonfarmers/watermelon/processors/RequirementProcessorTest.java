b533a24de77f8068a4f0b727124ac6e036b6238e
TargaryenQi <44879154+TargaryenQi@users.noreply.github.com>
2019-03-14 15:02:39 -0400

    Revert "Fix a test error"
    This reverts commit c53b60f87edeed7b699ec9ff2e7583ee7cbaba37.

--- src/test/java/com/watermelonfarmers/watermelon/processors/RequirementProcessorTest.java
+++ src/test/java/com/watermelonfarmers/watermelon/processors/RequirementProcessorTest.java
@@ -41,7 +41,7 @@ public class RequirementProcessorTest {
 
         ResponseEntity response = requirementProcessor.createRequirement(new Requirement());
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
